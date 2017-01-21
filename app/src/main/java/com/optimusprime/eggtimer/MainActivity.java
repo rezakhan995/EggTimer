88eccf647632e64ddf180bdbb8577802d784a8e8
Reza Khan <rezakhan995@gmail.com>
2017-01-22 02:54:46 +0600

    Initial commit

---
+++ app/src/main/java/com/optimusprime/eggtimer/MainActivity.java
@@ -0,0 +1,132 @@
+package com.optimusprime.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    SeekBar seekbar;
+    TextView timerTextView;
+    Button controllButton;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+
+    public void resetTimer() {
+
+        timerTextView.setText("0 : 00");
+        seekbar.setProgress(0);
+        countDownTimer.cancel();
+        controllButton.setText("GO!");
+        seekbar.setEnabled(true);
+        counterIsActive = false;
+    }
+
+    public void updateTimer(int secondsLeft) {
+
+        int minute = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minute * 60;
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+
+        timerTextView.setText(Integer.toString(minute) + ":" + secondString);
+
+    }
+
+    public void controllTimer(View view) {
+
+        if (counterIsActive == false) {
+
+            counterIsActive = true;
+            seekbar.setEnabled(false);
+            controllButton.setText("Stop");
+
+            countDownTimer = new CountDownTimer(seekbar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.horn);
+                    mediaPlayer.start();
+                }
+            }.start();
+        } else {
+            resetTimer();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        controllButton = (Button) findViewById(R.id.controllButton);
+        timerTextView = (TextView) findViewById(R.id.timeTextView);
+        seekbar = (SeekBar) findViewById(R.id.timerSeekBar);
+
+
+        seekbar.setMax(600);
+        seekbar.setProgress(0);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
